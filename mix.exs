1a34a8b6fd095894ee8f281aefc5a629546d5a4c
hizqeelrz <hizqeel1214@gmail.com>
2019-12-13 22:01:51 +0500

    add auth lib

--- mix.exs
+++ mix.exs
@@ -42,6 +42,8 @@ defmodule Bidhype.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:comeonin, "~> 5.1"},
+      {:argon2_elixir, "~> 2.0"},
       {:plug_cowboy, "~> 2.0"}
     ]
   end
