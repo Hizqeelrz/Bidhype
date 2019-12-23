96210ba7caeafbb7a6ccf73b82a992e219d8b346
hizqeelrz <hizqeel1214@gmail.com>
2019-12-23 21:41:40 +0500

    update bid show params

--- lib/bidhype_web/live/bid_live.ex
+++ lib/bidhype_web/live/bid_live.ex
@@ -1,22 +1,27 @@
 defmodule BidhypeWeb.BidLive do
   use Phoenix.LiveView
   alias BidhypeWeb.PageView
+  alias Bidhype.Auction
 
   def render(assigns) do
+    # IO.inspect assigns.bid
     PageView.render("bids.html", assigns)
-    # ~L"""
-    # <div>
-    #   <h1>$<%= @val %></h1>
-    #   <button phx-click="inc" class="btn btn-sm btn-success" id="reset">Bid</button>
-    # </div>
-    # """
   end
 
-  def mount(_session, socket) do
-    {:ok, assign(socket, val: 0)}
+  def mount(%{path_params: %{"id" => bid_id}}, socket) do
+    {:ok, assign(socket, :bid, Auction.get_bid!(bid_id))}
+  end
+
+  def handle_info({Auction, [:bid, _], _}, socket) do
+    {:noreply, socket}
   end
 
   def handle_event("inc", _val, socket) do
     {:noreply, update(socket, :val, &(&1 + 5))}
+  end
+
+  defp fetch(socket) do
+    # bid = Auction.get_bid!(id)
+    assign(socket, :bids, Auction.list_bids() )
   end
 end
